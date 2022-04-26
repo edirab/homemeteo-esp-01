eb2021d5caa40126e95dcb4c9f64408db1692031
Eugene Andreev <edirab@yandex.ru>
2022-04-26 16:11:18 +0300

    - В скрипте python запись в БД теперь происходит не раз в 2 секунды, а раз в 2 минуты. - Необходимо модифицировать схему БД, иначе слишком большой объём данных - Обнаружен баг в коде разбора строки на стороне esp 01: в поле lum2 попадают символы переноса и возврата каретки

--- mqtt_test.py
+++ mqtt_test.py
@@ -10,26 +10,49 @@ MQTT_CLIENT_ID = 'esp_1'
 MQTT_USER = 'esp'
 MQTT_PASSWORD = 'esp'
 TOPIC = 'home/#'
- 
+
+# В текущей реализации каждый параметр записывается отдельной строкой, 
+# что есть неоптимально. Схема базы данных будет переработана в будущем
+# Запись в БД будет происходить каждые 60 посылок, т.к. раз в 2 минуты
+N_RECORDS = 5 * 60
 DATABASE_FILE = 'mqtt.db'
- 
+
+messages = []
  
 def on_connect(mqtt_client, user_data, flags, conn_result):
     mqtt_client.subscribe(TOPIC)
  
- 
+
+def create_records(user_data):
+
+    if len(messages) > N_RECORDS:
+
+        sql = 'INSERT INTO sensors_data (topic, payload, created_at) VALUES (?, ?, ?)'
+        records = []
+
+        for msg in messages:
+            payload = msg.payload.decode('utf-8')
+            print("on_message", msg.topic, " : ", payload)
+            record = (msg.topic, payload, int(time()))
+            records.append(record)
+            # print("\n")
+
+        db_conn = user_data['db_conn']
+        
+        cursor = db_conn.cursor()
+        cursor.executemany(sql, records)
+        db_conn.commit()
+        cursor.close()
+
+        messages.clear()
+
+
 def on_message(mqtt_client, user_data, message):
-    payload = message.payload.decode('utf-8')
-    print("on_message", message.topic, " : ", payload)
- 
-    db_conn = user_data['db_conn']
-    sql = 'INSERT INTO sensors_data (topic, payload, created_at) VALUES (?, ?, ?)'
-    cursor = db_conn.cursor()
-    cursor.execute(sql, (message.topic, payload, int(time())))
-    db_conn.commit()
-    cursor.close()
- 
- 
+
+    messages.append(message)
+    create_records(user_data)
+
+
 def main():
     db_conn = sqlite3.connect(DATABASE_FILE)
     sql = """
